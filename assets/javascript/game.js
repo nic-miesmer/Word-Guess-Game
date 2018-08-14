45ed503664297feecfda488780ea2a983a26e502
nmiesmer <nic.miesmer@segmint.com>
2018-08-14 18:31:31 -0400

    added chooseWord function, and added it to gameSetup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,32 @@ var game = {
 
     wins: 0,
     words:["wheelbarrow", "friction", "companionship"],
+    chosenWord:"",
     guessedLetters: [],
     remainingGuesses: 20,
 
 
     gameSetup: function () {
+        this.chooseWord();       
+        
+        //do I remove the letter from chosen word,, and once it's empty win.
+        //do I keep a separate word fill array
+        
 
+    },
+
+    chooseWord: function(){
+
+        var rand = Math.floor(Math.random() * this.words.length);
+        //choose word.
+        this.chosenWord = this.words[rand];
+        console.log(this.chosenWord);
+        //Takes the index chosen, and removes it from the array
+        this.words.splice(rand, 1);
+        console.log(this.words);
+        //choose word randomly
+        //remove it from the words array
+        //if no words left congrats you finished! refresh to start again.
     },
 
     matchedLetter: function(){
@@ -38,7 +58,7 @@ var game = {
 
     },
 
-    revealLetter: function(leter){
+    revealLetter: function(letter){
         // find where the letter goes,
         // reveal it,
         //add letter to gussed letters
@@ -54,3 +74,5 @@ document.onkeyup = function (event) {
     //matchedLetter
 
 }
+
+game.gameSetup();
