111f977e303aeeb2a0479bfd2e8e0f4b79b9bdcd
nmiesmer <nic.miesmer@segmint.com>
2018-08-18 10:53:46 -0400

    on key up starts game, and reveals letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,17 @@
     var guessedLetters = [];
     var displayWord = [];
     var remainingGuesses = 20;
+    var isGameStart = false;
+    var isGameFinished = false;
 
       // Set the inner HTML contents of the #game div to our html string
 
    var gameSetup = function () {
+
+        var displayWord = [];
+        var remainingGuesses = 20;
+        var isGameFinished = false;
+
         chooseWord();       
         
         //do I remove the letter from chosen word,, and once it's empty win.
@@ -31,20 +38,21 @@
 
         displayWord = new Array(chosenWord.length).fill("_");
 
-        
-
-         
         console.log(displayWord);
         
-        
-        
+    
         //remove it from the words array
         //if no words left congrats you finished! refresh to start again.
     };
 
      var updateDisplay = function(){
  
-        var html = "<p>You chose: " + displayWord.join(" ") + "</p>";
+        var html = "<p>Guess This Word: " + displayWord.join(" ") + "</p>" +
+                   "<p>Guessed Letters: " + guessedLetters + "</p>" +
+                   "<p>Guesses Remaining: " + remainingGuesses + "</p>" +
+                   "<p>Games Won: " + wins + "</p>" ;
+
+        
 
       // Set the inner HTML contents of the #game div to our html string
       document.getElementById("game").innerHTML = html;
@@ -52,7 +60,18 @@
     };  
 
 
-   var  matchedLetter =  function(){
+   var  playerGuess =  function(letter){
+
+       if(guessedLetters.indexOf(letter) === -1){
+            addToGuessedLetters(letter);
+            revealLetter(letter);
+            remainingGuesses--;
+       }
+       updateDisplay();
+       checkGameStatus();
+
+
+    
         //Is the letter guessed in the word?
         //If it is
             //Reveal it in the word
@@ -63,30 +82,50 @@
             //push letter into guessed Letters array
     };
 
-    var revealLetter = function(){
+    var revealLetter = function(letter){
         // find where the letter goes,
         // reveal it,
         //add letter to gussed letters
     };
 
-    var addToGuessedLetters = function(){
+    var addToGuessedLetters = function(letter){
+        guessedLetters.push(letter);
         // add guessed letter to guessedLetters
     };
 
-    var gameEnd = function() {
+    var checkGameStatus = function() {
         //if all letters have been gussed and remaining guesses != 0
-            // Win
-                //restart game gameSetup()
-                // wins +1 
-        //else if
-            //remaining gusses = 0
-            // loss
-            //restart game -> gameSetup()
-        // else
-            // continue game
+
+        if(!displayWord.indexOf === -1 && remainingGuesses > 0){
+            gameWin();
+            console.log("yay you win");
+        }
+        else if(remainingGuesses <= 0){
+            console.log("Sorry you lose");
+        }
 
     };
-// document.onkeyup = function (event) {
-//     //matchedLetter
 
-// }
+
+    var gameWin = function() {
+        // wins +1 
+        //restart game gameSetup()
+        wins ++;
+        isGameFinished = true;
+
+    }
+
+    var gameLoss = function() {
+
+        isGameFinished = true;
+        //display something
+        
+    }
+
+    document.onkeyup = function (event) {
+
+        
+        playerGuess(event.key.toLowerCase());
+    }
+
+
